b20364b1e4e5aefd9003264b83a84e9022eaa9dc
AndreiSystem <andrei.tdaluz@gmail.com>
2019-12-23 23:55:32 -0300

    refeita versao 2

--- desafio/PAD-337(v2).py
+++ desafio/PAD-337(v2).py
@@ -1,42 +1,70 @@
 import random
-cartas = ["2","3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"]
+'''
+"2" => 2
+"3" => 3
+"4" => 4
+"5" => 5
+"6" => 6
+"7" => 7
+"8" => 8
+"9" => 9
+"10" => 10
+"A" => 1 or 11
+"J" => 10
+"Q" => 10
+"K" => 10
+'''
+def continuar():
+    while True:
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()
+        if resposta == 'S':
+            return True
+        elif resposta == 'N':
+            return False
+        else:
+            print('\033[1;31mResposta inválida, apenas [S/N]!\033[m')
+            continue
 
-print('JOGO DAS CARTAS!')
+
+cartas = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"]
+cartas_salvas = []
 
 pt = 0
-while pt < 21:
+
+continuar_resp = True
+while continuar_resp:
     random.shuffle(cartas)
-    carta = cartas.pop(random.randint(0, len(cartas)-1))
-    print(f'Carta: {carta}')
+
+    carta_retirada = cartas.pop(random.randint(0,len(cartas)-1))
+    cartas_salvas.append(carta_retirada)
+
+    print(f'Cartas já Viradas: {cartas_salvas}')
     try:
-        pt+=int(carta)
-        print(f'Pontuação: {pt}')
+        pt+=int(carta_retirada)
+        print(f'Pontuação: \033[1;32m{pt}\033[m')
     except:
-
-        if carta == 'A':
-
+        if carta_retirada == 'A':
             if pt > 0:
                 pt+=1
-                print(f'Pontuação: {pt}')
             else:
-                pt+=1
-                print(f'Pontuação: {pt}')
-        elif carta == 'J':
+                pt+=11
+        elif carta_retirada == 'J':
             pt+=10
-            print(f'Pontuação: {pt}')
-        elif carta == 'Q':
+        elif carta_retirada == 'Q':
             pt+=10
-            print(f'Pontuação: {pt}')
         else:
             pt+=10
-            print(f'Pontuação: {pt}')
-    if pt > 21:
-        break
-    resp = input('Deseja virar outra carta? [S/N] ').upper()
+        print(f'Pontuação: \033[1;32m{pt}\033[m')
 
-    if resp == 'S':
-        continue
-    else:
+    continuar_resp = continuar()
+    if len(cartas) == 0:
+        print('\033[1;33mOpa as cartas acabaram! Fim de jogo.\033[m')
         break
-print('Fim de Jogo!')
+    elif pt >= 21:
+        print('\033[1;33mOpa você passou dos 21 pontos! Fim de jogo.\033[m')
+        break
+    else:
+        continue
+
+print('Finalizado!')
 
